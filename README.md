9de8f36aedb1f1f16de2b3d50194448b34d61d22
Bob Lantz <rlantz@cs.stanford.edu>
2017-01-27 17:37:54 -0800

    Add build-onos.sh and CI integration

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 This is intended to be a simple script to demonstrate how to install
 ONOS from the .tar.gz archive on Ubuntu 16.04.
+
+[![Build Status][1]](https://travis-ci.org/lantz/install-onos)
+
+[1]: https://travis-ci.org/lantz/install-onos.svg?branch=master
---
+++ build-onos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+set -e  # exit on error
+
+echo "*** ONOS build script"
+
+echo "*** Fetching ONOS source code"
+builddir=onos-`date +%y%m%d-%H%M%S`
+mkdir $builddir
+cd $builddir
+git clone https://gerrit.onosproject.org/onos
+
+echo "*** Building ONOS"
+cd onos
+export ONOS_ROOT=  # in case it's set to point elsewhere
+echo "onos_root is $ONOS_ROOT"
+env | grep -i onos
+tools/build/onos-buck build onos
+
+echo "*** Linking output to ~/onos.tar.gz"
+ln -s buck-out/gen/tools/package/onos-package/onos.tar.gz ~
+
+echo "*** ONOS build complete"
+exit 0
