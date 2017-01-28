803191ce54eac0b0562058a35cb222623bb179ad
Bob Lantz <rlantz@cs.stanford.edu>
2017-01-28 15:52:51 -0800

    copy onos.tar.gz to top level of build directory
    We could symlink it, but copying it will fail if the file
    doesn't exist! Also it is possible to delete the onos/
    directory afterward, should we desire to do so.

--- build-onos.sh
+++ build-onos.sh
@@ -11,14 +11,15 @@ cd $builddir
 git clone https://gerrit.onosproject.org/onos
 
 echo "*** Building ONOS"
-cd onos
+pushd onos
 export ONOS_ROOT=  # in case it's set to point elsewhere
 echo "onos_root is $ONOS_ROOT"
 env | grep -i onos
 tools/build/onos-buck build onos
+popd
 
-echo "*** Linking output to ~/onos.tar.gz"
-ln -s buck-out/gen/tools/package/onos-package/onos.tar.gz ~
+echo "*** Copying onos.tar.gz to" `pwd`
+cp onos/buck-out/gen/tools/package/onos-package/onos.tar.gz .
 
 echo "*** ONOS build complete"
 exit 0
