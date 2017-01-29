1797ada84b108eb0c60453437ae972167f113e0b
Bob Lantz <rlantz@cs.stanford.edu>
2017-01-28 16:10:35 -0800

    Date/time-tagged names for $builddir and .tar.gz

--- build-onos.sh
+++ build-onos.sh
@@ -7,19 +7,18 @@ echo "*** ONOS build script"
 echo "*** Fetching ONOS source code"
 builddir=onos-`date +%y%m%d-%H%M%S`
 mkdir $builddir
-cd $builddir
-git clone https://gerrit.onosproject.org/onos
+git clone https://gerrit.onosproject.org/onos $builddir
 
 echo "*** Building ONOS"
-pushd onos
+pushd $builddir
 export ONOS_ROOT=  # in case it's set to point elsewhere
 echo "onos_root is $ONOS_ROOT"
 env | grep -i onos
 tools/build/onos-buck build onos
 popd
 
-echo "*** Copying onos.tar.gz to" `pwd`
-cp onos/buck-out/gen/tools/package/onos-package/onos.tar.gz .
+echo "*** Copying onos.tar.gz to onos-$tag.tar.gz"
+cp onos-$tag/buck-out/gen/tools/package/onos-package/onos.tar.gz onos-$tag.tar.gz
 
 echo "*** ONOS build complete"
 exit 0
