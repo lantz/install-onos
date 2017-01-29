599b009105b4a884ad2e9b524eec4e9fe55c448f
Bob Lantz <rlantz@cs.stanford.edu>
2017-01-28 18:59:45 -0800

    Mininet master fixes problem; removing debug output

--- test-onos.sh
+++ test-onos.sh
@@ -6,9 +6,6 @@ echo "*** Installing mininet"
 git clone git://github.com/mininet/mininet
 mininet/util/install.sh -nv > /dev/null
 
-echo "*** Checking OVS status"
-service openvswitch-switch status
-
 echo "*** Checking ONOS status"
 service onos status < /dev/null
 ps ax | grep java
@@ -26,8 +23,7 @@ for i in `seq 1 90`; do
 done
 
 echo "*** Confirming that ONOS is listening on port 6653"
-netstat -atp
 netstat -atp | egrep ':6653'
 
 echo "*** Testing ONOS"
-mn -v debug --controller remote,port=6653 --test pingall
+mn --controller remote,port=6653 --test pingall
