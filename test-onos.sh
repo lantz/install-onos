a3b0b47dd5da05fdca765e0ebef0b5b2f457bb10
Bob Lantz <rlantz@cs.stanford.edu>
2017-01-28 17:29:08 -0800

    Dump onos options; increase timeout to 90s; verify listener

--- test-onos.sh
+++ test-onos.sh
@@ -8,14 +8,20 @@ apt-get -qq install mininet
 echo "*** Checking ONOS status"
 service onos status
 
+echo "*** Dumping ONOS options"
+cat /opt/onos/options
+
 echo "*** Waiting for ONOS openflow app startup"
-for i in `seq 1 30`; do
+for i in `seq 1 90`; do
   if netstat -atp | egrep ':6653'; then
     break
   fi
   echo -n .
   sleep 1
 done
+
+echo "*** Confirming that ONOS is listening on port 6653"
+netstat -atp | egrep ':6653'
 
 echo "*** Testing ONOS"
 mn --controller remote,port=6653 --test pingall
