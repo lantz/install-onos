be2bc975b211545ea633f70de40ad14bc8a91dba
Bob Lantz <rlantz@cs.stanford.edu>
2017-01-28 16:40:31 -0800

    Wait for ONOS startup

--- test-onos.sh
+++ test-onos.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-set -e
+set -e  # Exit on any error
 
 echo "*** Installing mininet"
-apt-get -y install mininet
+apt-get -y install mininet >/dev/null
 
-echo "*** Running netstat"
-netstat -atp | egrep ':6653|:6633'
+echo "*** Checking ONOS status"
+service onos status
+
+echo "*** Waiting for ONOS openflow app startup"
+for i in `seq 1 30`; do
+  if netstat -atp | egrep ':6653'; then
+    break
+  fi
+  echo -n .
+  sleep 1
+done
 
 echo "*** Testing ONOS"
 mn --controller remote,port=6653 --test pingall
