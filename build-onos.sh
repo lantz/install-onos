e6b904d1592886e41e20ed207b839a745effc557
Bob Lantz <rlantz@cs.stanford.edu>
2017-01-30 14:47:14 -0800

    Add unit tests to build-onos.sh

--- build-onos.sh
+++ build-onos.sh
@@ -9,13 +9,19 @@ builddir=onos-`date +%y%m%d-%H%M%S`
 mkdir $builddir
 git clone https://gerrit.onosproject.org/onos $builddir
 
-echo "*** Building ONOS"
 pushd $builddir
+
+echo "*** Building ONOS"
 export ONOS_ROOT=  # in case it's set to point elsewhere
 echo "onos_root is $ONOS_ROOT"
 env | grep -i onos
 tools/build/onos-buck build onos 1>/dev/null
+
+echo "*** Running ONOS unit tests"
+tools/build/onos-buck test
+
 popd
+
 
 echo "*** Copying onos.tar.gz to onos-$tag.tar.gz"
 cp $builddir/buck-out/gen/tools/package/onos-package/onos.tar.gz onos-$tag.tar.gz
